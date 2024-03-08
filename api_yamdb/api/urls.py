af10b8a0f415c6ca34ecb01a91cf08c52a925705
Sergey Kuroedov <sergeylive87@yandex.ru>
2024-03-08 13:02:17 +0300

    Add in routers categories and genres

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,9 +2,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
+from api.views import CategoryViewSet, GenreViewSet
 
 router_v1 = DefaultRouter()
 
+router_v1.register(r'categories', CategoryViewSet, basename='category')
+router_v1.register(r'genres', GenreViewSet, basename='genre')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
