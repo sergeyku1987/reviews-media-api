7122db1234c9d440e1f499fc0b628d63d641e945
Sergey Kuroedov <sergeylive87@yandex.ru>
2024-03-08 12:37:54 +0300

    Add CategorySerializer and GenreSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,1 +1,15 @@
 from rest_framework import serializers
+
+from reviews.models import Category, Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
