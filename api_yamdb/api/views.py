a31ef559de21d41f414b42c13f1e0efccc1f03d8
Sergey Kuroedov <sergeylive87@yandex.ru>
2024-03-08 12:33:57 +0300

    Add CategoryViewSet and GenreViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from reviews.models import Category, Genre
+from api.serializers import CategorySerializer, GenreSerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre
+    serializer_class = GenreSerializer
