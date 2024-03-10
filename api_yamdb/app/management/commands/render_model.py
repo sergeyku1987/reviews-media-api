8c16b46c5efdc6094ebee2f7e84c3d136bfb6486
Sergey <sergey@sergey.com>
2024-03-10 17:49:42 +0300

    Prototype command render_model

--- api_yamdb/app/management/commands/render_model.py
+++ api_yamdb/app/management/commands/render_model.py
@@ -1,41 +1,59 @@
 from django.core.management.base import BaseCommand, CommandParser
 from pathlib import Path
 import os
+import csv
 
+from reviews.models import Category
 
 class Command(BaseCommand):
 
     def  handle(self, *args, **options):
-        dr = options.get('name_dir')[0]
-        fl = options.get('name_file')[0]
-        pt = ''
-        for path, dirname, _ in os.walk(Path.cwd()):
-            print('---')
-            if dr in dirname:
-                pt = Path(path) / dr
-                break
-        for file in os.listdir(pt):
-            if file == fl:
-                print('find')
-                break
+        print(options)
+        dr = options.get('name_dir')
+        fl = options.get('name_file')
+        md =  options.get('model')
+        path_to_file = ''
+
+        if dr:
+            for path, dirname, _ in os.walk(Path.cwd()):
+                if dr in dirname:
+                    path_to_file = Path(path) / dr
+                    break
+
+        if os.path.exists(path_to_file):
+            for file in os.listdir(path_to_file):
+                if file == fl:
+                    with open(Path(path_to_file) / fl, encoding='utf-8') as file:
+                        example_dict = csv.DictReader(file)
+                        for i in example_dict:
+                            try:
+                                Category.objects.create(**i)
+                            except:
+                                pass                        
+                    print('finish')
+                    break
+        
         
 
     def add_arguments(self, parser):
-        # Positional arguments
-        parser.add_argument('name_dir', nargs='+', type=str)
 
         # Named (optional) arguments
         parser.add_argument(
             '-d',
-            action='store_true',
+            action='store',
+            dest='name_dir',
         )
-
-        # Positional arguments
-        parser.add_argument('name_file', nargs='+', type=str)
 
         # Named (optional) arguments
         parser.add_argument(
             '-f',
-            action='store_true',
+            action='store',
+            dest='name_file'
+        )
+
+        parser.add_argument(
+            '-m',
+            action='store',
+            dest='model',
         )
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
@@ -9,3 +9,50 @@ class Category(models.Model):
 class Genre(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.PositiveSmallIntegerField()
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.PROTECT,
+    )
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        to=Title,
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.PROTECT
+    )
+    score = models.PositiveSmallIntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        to=Review,
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.PROTECT,
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        to=Title,
+        on_delete=models.PROTECT,
+    )
+    genre = models.ForeignKey(
+        to=Genre,
+        on_delete=models.PROTECT
+    )
